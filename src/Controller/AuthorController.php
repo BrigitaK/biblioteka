51104e9fe064dab3e5a149547e4599d1bf4203a1
BrigitaK <70912981+BrigitaK@users.noreply.github.com>
2021-02-02 14:36:51 +0200

    validation

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -41,7 +41,8 @@ class AuthorController extends AbstractController
         
         return $this->render('author/index.html.twig', [
             'authors' => $authors,
-            'sortBy' => $r->query->get('sort') ?? 'default'
+            'sortBy' => $r->query->get('sort') ?? 'default',
+            'success' => $r->getSession()->getFlashBag()->get('success', [])
         ]);
     }
     /**
@@ -49,8 +50,14 @@ class AuthorController extends AbstractController
      */
     public function create(Request $r): Response
     {
+
+        $author_name = $r->getSession()->getFlashBag()->get('author_name', []);
+        $author_surname = $r->getSession()->getFlashBag()->get('author_surname', []);
+
         return $this->render('author/create.html.twig', [
-            'errors' => $r->getSession()->getFlashBag()->get('errors', [])
+            'errors' => $r->getSession()->getFlashBag()->get('errors', []),
+            'author_name' => $author_name[0] ?? '',
+            'author_surname' => $author_surname[0] ?? ''
         ]);
     }
     /**
@@ -59,6 +66,7 @@ class AuthorController extends AbstractController
     public function store(Request $r, ValidatorInterface $validator): Response
     {
         $author = New Author;
+        
         $author->
         setName($r->request->get('author_name'))->
         setSurname($r->request->get('author_surname'));
@@ -71,15 +79,21 @@ class AuthorController extends AbstractController
             foreach($errors as $error) {
                 $r->getSession()->getFlashBag()->add('errors', $error->getMessage());
             }
+            $r->getSession()->getFlashBag()->add('author_name', $r->request->get('author_name'));
+            $r->getSession()->getFlashBag()->add('author_surname', $r->request->get('author_surname'));
+
             return $this->redirectToRoute('author_create');
         }
 
+    
         //susikuriam manager
         $entityManager = $this->getDoctrine()->getManager();
         //i ta manager persiunciam author
         $entityManager->persist($author);
         //ir viska irasom i duomenu baze
         $entityManager->flush();
+
+        $r->getSession()->getFlashBag()->add('success', 'autorius sekmingai prideras');
 
         //grazinu redirect
         return $this->redirectToRoute('author_index');
@@ -88,20 +102,27 @@ class AuthorController extends AbstractController
      /**
      * @Route("/author/edit/{id}", name="author_edit", methods={"GET"})
      */
-    public function edit(int $id): Response
+    public function edit(int $id, Request $r): Response
     {
         $author = $this->getDoctrine()
         ->getRepository(Author::class)
         ->find($id);
 
+        $author_name = $r->getSession()->getFlashBag()->get('author_name', []);
+        $author_surname = $r->getSession()->getFlashBag()->get('author_surname',[]);
+
         return $this->render('author/edit.html.twig', [
             'author' => $author,
+            'errors' => $r->getSession()->getFlashBag()->get('errors', []),
+            'author_name' => $author_name[0] ?? '',
+            'author_surname' => $author_surname[0] ?? ''
+
         ]);
     }
        /**
      * @Route("/author/update/{id}", name="author_update", methods={"POST"})
      */
-    public function update(Request $r, $id): Response
+    public function update(Request $r, $id, ValidatorInterface $validator): Response
     {
         $author = $this->getDoctrine()
         ->getRepository(Author::class)
@@ -111,9 +132,25 @@ class AuthorController extends AbstractController
         setName($r->request->get('author_name'))->
         setSurname($r->request->get('author_surname'));
 
+        $errors = $validator->validate($author);
+
+
+        if (count($errors) > 0) {
+
+            foreach($errors as $error) {
+                $r->getSession()->getFlashBag()->add('errors', $error->getMessage());
+            }
+            $r->getSession()->getFlashBag()->add('author_name', $r->request->get('author_name'));
+            $r->getSession()->getFlashBag()->add('author_surname', $r->request->get('author_surname'));
+
+            return $this->redirectToRoute('author_edit', ['id' => $author->getId()]);
+        }
+
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->persist($author);
         $entityManager->flush();
+
+        $r->getSession()->getFlashBag()->add('success', 'autorius sekmingai pakeistas');
 
         //grazinu redirect
         return $this->redirectToRoute('author_index');
